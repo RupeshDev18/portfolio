eed314b811561b67ebe11eaa3dabf9ab02f77dfc
Rupesh <112386480+Rupesh180902@users.noreply.github.com>
2023-09-25 09:21:51 +0530

    profile updated

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -10,14 +10,20 @@ export default function Experience() {
         <div className="timeline-block timeline-block-right">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2020-2021</h3>
+            <h3>2022-2022</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-              deserunt aliquam cupiditate maxime modi voluptates molestiae
-              commodi quis tempora odio alias, quibusdam minus doloremque totam
-              omnis eaque reiciendis, dolores sint! Possimus, ut praesentium
-              laudantium cupiditate nemo saepe et suscipit amet eaque aliquid
-              facere at, id nihil consectetur ipsam!
+              <b>Volunteer</b> | <b>PU Projections</b>
+              <br />I volunteered at <strong>PU projections</strong>, Gujarat's
+              largest Tech Fest, where I managed a crowd of over 200
+              participants during the CodeHunt event. My responsibilities
+              included event coordination, crowd management, and providing
+              technical support. Additionally, I played a pivotal role in
+              designing challenging C and C++ questions for the competition.
+              This experience allowed me to contribute to the success of the
+              event, hone my event management skills, and promote a spirit of
+              learning and collaboration among tech enthusiasts. I eagerly
+              anticipate future opportunities to engage in such impactful
+              initiatives.
             </p>
           </div>
         </div>
@@ -27,14 +33,20 @@ export default function Experience() {
         <div className="timeline-block timeline-block-left">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2021-2022</h3>
+            <h3>2023-2023</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-              deserunt aliquam cupiditate maxime modi voluptates molestiae
-              commodi quis tempora odio alias, quibusdam minus doloremque totam
-              omnis eaque reiciendis, dolores sint! Possimus, ut praesentium
-              laudantium cupiditate nemo saepe et suscipit amet eaque aliquid
-              facere at, id nihil consectetur ipsam!
+              <b>Nodejs Intern </b> | <b>PanvaTech</b>
+              <br />
+              During my internship at <strong>PanvaTech</strong>, I had the
+              privilege of working with a skilled Node.js development team. I
+              actively contributed to building and optimizing server-side
+              applications, creating efficient RESTful APIs, and mastering the
+              art of debugging and troubleshooting. My experience also included
+              integrating diverse data storage solutions like MySQL and MongoDB,
+              honing my abilities in data management. This internship not only
+              deepened my Node.js expertise but also exposed me to industry best
+              practices and a collaborative work culture, making it a pivotal
+              step in my journey as a developer
             </p>
           </div>
         </div>
@@ -42,20 +54,24 @@ export default function Experience() {
         <div className="timeline-block timeline-block-right">
           <div className="marker"></div>
           <div className="timeline-content">
-            <h3>2023-2023</h3>
+            <h3>2023-Present</h3>
             <p>
-              <b>Nodejs Intern</b>
+              <b>Teaching Assistant </b> | <b>Parul University</b>
               <br />
-              During my internship, I had the privilege of working with a
-              skilled Node.js development team. I actively contributed to
-              building and optimizing server-side applications, creating
-              efficient RESTful APIs, and mastering the art of debugging and
-              troubleshooting. My experience also included integrating diverse
-              data storage solutions like MySQL and MongoDB, honing my abilities
-              in data management. This internship not only deepened my Node.js
-              expertise but also exposed me to industry best practices and a
-              collaborative work culture, making it a pivotal step in my journey
-              as a developer
+              As a Teaching Assistant at <strong>Parul University</strong>, I've
+              focused on imparting my expertise in programming languages,
+              Database Management Systems (DBMS), and Operating Systems (OS). My
+              approach blends theoretical knowledge with hands-on practice,
+              ensuring that students gain a comprehensive understanding of these
+              crucial facets of computer science. In the realm of programming, I
+              emphasize problem-solving and practical application, equipping
+              students with proficiency in multiple languages. In the DBMS
+              domain, I delve into data modeling, query optimization, and
+              database design, preparing students to handle complex data
+              structures. In the context of Operating Systems, I explore
+              fundamental concepts such as process management and memory
+              allocation, enabling students to grasp the foundational
+              infrastructure of computing.
             </p>
           </div>
         </div>
